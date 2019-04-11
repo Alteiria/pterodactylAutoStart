769fbcfc54620ba1e08bc1729610665aede39d63
Emilien Devos <contact@emiliendevos.be>
2019-04-11 22:07:55 +0200

    working

---
+++ autoRestart.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [[ -z ${baseURL} || -z ${apiToken} ]]; then
+    echo "Please set baseURL+/api to your Pterodactyl panel URL. Example: https://panel.minecraft.com/api."
+    echo "Please set the API Token according to Application API section in the administration."
+    exit 1
+fi
+
+dataOfServers=$(curl -s "${baseURL}""/client" \
+    -H "Authorization: Bearer $apiToken" \
+    -H "Content-Type: application/json" \
+-H "Accept: Application/vnd.pterodactyl.v1+json" | jq '.data')
+
+numberOfServers=$(echo "${dataOfServers}" | jq length)
+
+for (( i=0; i < "$numberOfServers"; i++ ))
+do
+    dataOfServer=$(echo "${dataOfServers}" | jq ".[$i]")
+    description=$(echo "${dataOfServer}" | jq ".attributes.description" | jq -r)
+	uuid=$(echo "$dataOfServer" | jq ".attributes.uuid" | jq -r)
+	id=${uuid::8}
+    if [[ $description == *"alwaysStart"* ]]; then
+        curl -s "$baseURL""/client/servers/${id}/power" \
+        -H "Authorization: Bearer $apiToken" \
+        -H "Content-Type: application/json" \
+        -H "Accept: Application/vnd.pterodactyl.v1+json" \
+        -X POST \
+        -d '{ "signal": "start" }'
+    fi
+done
